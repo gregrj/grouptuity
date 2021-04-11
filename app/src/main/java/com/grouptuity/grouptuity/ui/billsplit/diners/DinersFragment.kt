a813e03132e8aa570a1e4e3fc508a47fe818d742
gregrj <gregrj@users.noreply.github.com>
2021-04-11 19:42:00 -0400

    RecyclerView for Diners Fragment
    - LiveData for diners and subtotal list in the view model
    - Added RecyclerView and adapter for Diners
    - Created a bottom offset decoration so last item is not covered by the FAB

--- app/src/main/java/com/grouptuity/grouptuity/ui/billsplit/diners/DinersFragment.kt
+++ app/src/main/java/com/grouptuity/grouptuity/ui/billsplit/diners/DinersFragment.kt
@@ -1,12 +1,20 @@
 package com.grouptuity.grouptuity.ui.billsplit.diners
 
+import android.content.Context
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
 import com.grouptuity.grouptuity.R
+import com.grouptuity.grouptuity.data.Diner
+import com.grouptuity.grouptuity.databinding.FragDinersListitemBinding
+import com.grouptuity.grouptuity.ui.custom.RecyclerViewListener
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class DinersFragment : Fragment() {
 
@@ -26,4 +34,57 @@ class DinersFragment : Fragment() {
         // TODO: Use the ViewModel
     }
 
+}
+
+class DinersRecyclerViewAdapter(private val context: Context,
+                                private val listener: RecyclerViewListener): RecyclerView.Adapter<DinersRecyclerViewAdapter.ViewHolder>() {
+
+    private var mDataSet = emptyList<Pair<Diner,String>>()
+
+    inner class ViewHolder(val viewBinding: FragDinersListitemBinding): RecyclerView.ViewHolder(viewBinding.root) {
+        init {
+            itemView.setOnClickListener(listener)
+            itemView.setOnLongClickListener(listener)
+        }
+    }
+
+    override fun getItemCount() = mDataSet.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+            ViewHolder(FragDinersListitemBinding.inflate(LayoutInflater.from(context), parent, false))
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val newDiner = mDataSet[position]
+
+        // TODO
+    }
+
+    suspend fun updateDataSet(newDataSet: List<Pair<Diner, String>>) {
+        val adapter = this
+
+        val diffResult = DiffUtil.calculateDiff(object: DiffUtil.Callback() {
+            override fun getOldListSize() = mDataSet.size
+
+            override fun getNewListSize() = newDataSet.size
+
+            override fun areItemsTheSame(oldPosition: Int, newPosition: Int) = newDataSet[newPosition].first.id == mDataSet[oldPosition].first.id
+
+            override fun areContentsTheSame(oldPosition: Int, newPosition: Int): Boolean {
+                val newDiner = newDataSet[newPosition].first
+                val oldDiner = mDataSet[oldPosition].first
+
+                return newDiner.id == oldDiner.id &&
+                        newDiner.name == oldDiner.name &&
+                        newDiner.photoUri == oldDiner.photoUri &&
+                        newDiner.items.size == oldDiner.items.size &&
+                        newDataSet[newPosition].second == mDataSet[oldPosition].second
+            }
+        })
+
+        withContext(Dispatchers.Main) {
+            adapter.notifyItemChanged(mDataSet.size - 1) // clears BottomOffset from old last item
+            mDataSet = newDataSet
+            diffResult.dispatchUpdatesTo(adapter)
+        }
+    }
 }
--- app/src/main/java/com/grouptuity/grouptuity/ui/billsplit/diners/DinersViewModel.kt
+++ app/src/main/java/com/grouptuity/grouptuity/ui/billsplit/diners/DinersViewModel.kt
@@ -1,7 +1,30 @@
 package com.grouptuity.grouptuity.ui.billsplit.diners
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.asLiveData
+import com.grouptuity.grouptuity.data.Diner
+import com.grouptuity.grouptuity.data.Repository
+import kotlinx.coroutines.flow.combine
+import java.text.NumberFormat
 
-class DinersViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class DinersViewModel(application: Application): AndroidViewModel(application) {
+    private val repository = Repository.getInstance(application)
+    private val formatter = NumberFormat.getCurrencyInstance()
+
+    // Diners paired with their individual subtotals as currency strings
+    val dinerData: LiveData<List<Pair<Diner, String>>> = combine(repository.diners, repository.dinerSubtotals) { diners, subtotals ->
+        diners.map { diner ->
+            diner to formatter.format(subtotals[diner.id])
+        }
+    }.asLiveData()
+
+    fun removeDiner(diner: Diner) {
+        // TODO repository.deleteDiner(diner)
+    }
+
+    fun addSelfToBill() {
+        // TODO repository.addSelfToBill()
+    }
 }
