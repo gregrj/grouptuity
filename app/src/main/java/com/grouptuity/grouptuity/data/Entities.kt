9c140324a6809f2bc22b9cd8d4fd25898e4e12e9
gregrj <gregrj@users.noreply.github.com>
2021-04-10 22:38:09 -0400

    SQL join tables for model entities

--- app/src/main/java/com/grouptuity/grouptuity/data/Entities.kt
+++ app/src/main/java/com/grouptuity/grouptuity/data/Entities.kt
@@ -370,3 +370,67 @@ data class Payment(@PrimaryKey(autoGenerate = true) val id: Long,
                    val committed: Boolean,
                    val payerId: Long,
                    val payeeId: Long)
+
+
+@Entity(tableName = "diner_item_join_table",
+    primaryKeys = ["dinerId", "itemId"],
+    foreignKeys = [ForeignKey(entity = Diner::class, parentColumns = ["id"], childColumns = ["dinerId"], onDelete = ForeignKey.CASCADE),
+        ForeignKey(entity = Item::class,  parentColumns = ["id"], childColumns = ["itemId"], onDelete = ForeignKey.CASCADE)],
+    indices = [Index("itemId")])
+data class DinerItemJoin(val dinerId: Long, val itemId: Long)
+
+
+@Entity(tableName = "debt_debtor_join_table",
+    primaryKeys = ["debtId", "dinerId"],
+    foreignKeys = [ForeignKey(entity = Debt::class,  parentColumns = ["id"], childColumns = ["debtId"], onDelete = ForeignKey.CASCADE),
+        ForeignKey(entity = Diner::class, parentColumns = ["id"], childColumns = ["dinerId"], onDelete = ForeignKey.CASCADE)],
+    indices = [Index("dinerId")])
+data class DebtDebtorJoin(val debtId: Long, val dinerId: Long)
+
+
+@Entity(tableName = "debt_creditor_join_table",
+    primaryKeys = ["debtId", "dinerId"],
+    foreignKeys = [ForeignKey(entity = Debt::class,  parentColumns = ["id"], childColumns = ["debtId"], onDelete = ForeignKey.CASCADE),
+        ForeignKey(entity = Diner::class, parentColumns = ["id"], childColumns = ["dinerId"], onDelete = ForeignKey.CASCADE)],
+    indices = [Index("dinerId")])
+data class DebtCreditorJoin(val debtId: Long, val dinerId: Long)
+
+
+@Entity(tableName = "discount_recipient_join_table",
+    primaryKeys = ["discountId", "dinerId"],
+    foreignKeys = [ForeignKey(entity = Discount::class,  parentColumns = ["id"], childColumns = ["discountId"], onDelete = ForeignKey.CASCADE),
+        ForeignKey(entity = Diner::class, parentColumns = ["id"], childColumns = ["dinerId"], onDelete = ForeignKey.CASCADE)],
+    indices = [Index("dinerId")])
+data class DiscountRecipientJoin(val discountId: Long, val dinerId: Long)
+
+
+@Entity(tableName = "discount_purchaser_join_table",
+    primaryKeys = ["discountId", "dinerId"],
+    foreignKeys = [ForeignKey(entity = Discount::class,  parentColumns = ["id"], childColumns = ["discountId"], onDelete = ForeignKey.CASCADE),
+        ForeignKey(entity = Diner::class, parentColumns = ["id"], childColumns = ["dinerId"], onDelete = ForeignKey.CASCADE)],
+    indices = [Index("dinerId")])
+data class DiscountPurchaserJoin(val discountId: Long, val dinerId: Long)
+
+
+@Entity(tableName = "discount_item_join_table",
+    primaryKeys = ["discountId", "itemId"],
+    foreignKeys = [ForeignKey(entity = Discount::class,  parentColumns = ["id"], childColumns = ["discountId"], onDelete = ForeignKey.CASCADE),
+        ForeignKey(entity = Item::class, parentColumns = ["id"], childColumns = ["itemId"], onDelete = ForeignKey.CASCADE)],
+    indices = [Index("itemId")])
+data class DiscountItemJoin(val discountId: Long, val itemId: Long)
+
+
+@Entity(tableName = "payment_payer_join_table",
+    primaryKeys = ["paymentId", "dinerId"],
+    foreignKeys = [ForeignKey(entity = Payment::class,  parentColumns = ["id"], childColumns = ["paymentId"], onDelete = ForeignKey.CASCADE),
+        ForeignKey(entity = Diner::class, parentColumns = ["id"], childColumns = ["dinerId"], onDelete = ForeignKey.CASCADE)],
+    indices = [Index("dinerId")])
+data class PaymentPayerJoin(val paymentId: Long, val dinerId: Long)
+
+
+@Entity(tableName = "payment_payee_join_table",
+    primaryKeys = ["paymentId", "dinerId"],
+    foreignKeys = [ForeignKey(entity = Payment::class,  parentColumns = ["id"], childColumns = ["paymentId"], onDelete = ForeignKey.CASCADE),
+        ForeignKey(entity = Diner::class, parentColumns = ["id"], childColumns = ["dinerId"], onDelete = ForeignKey.CASCADE)],
+    indices = [Index("dinerId")])
+data class PaymentPayeeJoin(val paymentId: Long, val dinerId: Long)
